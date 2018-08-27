5ee464ea8f3c11356b7f6fa03b4cba83b7fd868a
CReator <kpeamop@gmail.com>
2018-08-28 01:45:43 +0300

    функции для работы с датами

--- webface/js/functions.js
+++ webface/js/functions.js
@@ -1,12 +1,23 @@
-function totime(tick)
+function totime(ts)
 {
-	var second=tick%60;
-	var minute=Math.floor(tick/=60)%60;
-	var hour=Math.floor(tick/=60)%60;
-	var day=Math.floor(tick/24);
+	var second=ts%60;
+	var minute=Math.floor(ts/=60)%60;
+	var hour=Math.floor(ts/=60)%60;
+	var day=Math.floor(ts/24);
 
 	return 	(day>0 ? day+' ' : '')+
 			(hour<10 ? '0'+hour : hour)+':'+
 			(minute<10 ? '0'+minute : minute)+':'+
 			(second<10 ? '0'+second : second);
 };
+
+function timeFormat(ts,format)
+{
+	format=format || 'y-m-d h:i:s';
+
+	var d=new Date(ts*1000);
+
+	var dt=s => (i => { s=='Month' && i++; return i<10 ? '0'+i : i; })(d['get'+s]());
+
+	return format.replace(/[ymdhis]/g,i => dt({y:'FullYear',m:'Month',d:'Date',h:'Hours',i:'Minutes',s:'Seconds'}[i]) );
+}
